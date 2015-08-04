2ea2810c2c00ce233219ffd94b193263c1409997
Chibisi Chima-Okereke <chibisi@activeanalytics.co.uk>
2015-08-04 13:22:14 +0100

    Update mongo_rcpp_2.cpp
    Fixed code now can write data frames containing character and numeric columns to mongodb database

--- mongo_rcpp_2.cpp
+++ mongo_rcpp_2.cpp
@@ -12,6 +12,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <set>
 #include <iomanip>
 
 using namespace Rcpp;
@@ -21,247 +22,157 @@ using mongo::BSONObjBuilder;
 
 // g++ mongo_rcpp_2.cpp -pthread -lmongoclient -lboost_thread -lboost_system -lboost_regex -o mongo_rcpp_2
 
-// Function to check the sexp type
+
+// Writes a data frame to a mongo db collection
 // [[Rcpp::export]]
-SEXP check_type(SEXP x){
-	if(TYPEOF(x) == STRSXP){
-		const char* _print = CHAR(Rf_asChar(x));
-		std::cout << _print << " is a string" << std::endl;
-	}else{
-		std::cout << "This is not a string!" << std::endl;
-	}
-	return wrap(0);
-}
-
-// Creates BSONObj with single BSONElement and returns it
-BSONObj sexp_to_bson(SEXP field_name, SEXP field_value){
-	const char* _field_name = CHAR(Rf_asChar(field_name));
-	BSONObj _p;
-	BSONObjBuilder b;
-	if(TYPEOF(field_value) == REALSXP){
-			double numeric_field = Rf_asReal(field_value);
-			b.append(_field_name, numeric_field);
-	} else if(TYPEOF(field_value) == INTSXP){
-			int int_field = Rf_asInteger(field_value);
-			b.append(_field_name, int_field);
-	} else if(TYPEOF(field_value) == LGLSXP){
-			int int_field = Rf_asLogical(field_value);
-			b.append(_field_name, int_field);
-	} else { // catch all convert to const char
-			const char* char_field = CHAR(Rf_asChar(field_value));
-			b.append(_field_name, char_field);
-	}
-	_p = b.obj();
-	return(_p);
-}
-
-BSONObj sexp_to_bson_obj(SEXP field_name, SEXP field_value){
-	const char* _field_name = CHAR(Rf_asChar(field_name));
-	BSONObj _p;
-	BSONObjBuilder b;
-	if(TYPEOF(field_value) == REALSXP){
-		NumericVector _field(field_value);
-		for(int i = 0; i < _field.size(); ++i){
-			b.append(_field_name, _field[i]);
-		}
-	} else if(TYPEOF(field_value) == INTSXP){
-		IntegerVector _field(field_value);
-		for(int i = 0; i < _field.size(); ++i){
-			b.append(_field_name, _field[i]);
-		}
-	} else if(TYPEOF(field_value) == LGLSXP){
-		IntegerVector _field(field_value);
-		for(int i = 0; i < _field.size(); ++i){
-			b.append(_field_name, _field[i]);
-		}
-	} else { // catch all convert to const char
-		CharacterVector _field(field_value);
-		const char* _el;
-		for(int i = 0; i < _field.size(); ++i){
-			_el = _field[i];
-			b.append(_field_name, _el);
-		}
-	}
-	_p = b.obj();
-	return(_p);
-}
-
-// Overload
-BSONObj sexp_to_bson_obj(const char* _field_name, SEXP field_value){
-	BSONObj _p;
-	BSONObjBuilder b;
-	if(TYPEOF(field_value) == REALSXP){
-		NumericVector _field(field_value);
-		for(int i = 0; i < _field.size(); ++i){
-			b.append(_field_name, _field[i]);
-		}
-	} else if(TYPEOF(field_value) == INTSXP){
-		IntegerVector _field(field_value);
-		for(int i = 0; i < _field.size(); ++i){
-			b.append(_field_name, _field[i]);
-		}
-	} else if(TYPEOF(field_value) == LGLSXP){
-		IntegerVector _field(field_value);
-		for(int i = 0; i < _field.size(); ++i){
-			b.append(_field_name, _field[i]);
-		}
-	} else { // catch all convert to const char
-		CharacterVector _field(field_value);
-		const char* _el;
-		for(int i = 0; i < _field.size(); ++i){
-			_el = _field[i];
-			b.append(_field_name, _el);
-		}
-	}
-	_p = b.obj();
-	return(_p);
-}
-
-
-// Test function
-BSONObj bson_out(){
-	BSONObj p;
-	BSONObjBuilder b;
-	b.append("foo", "bar");
-	p = b.obj();
-	return p;
-}
-
-// Test BSON Output (string data only)
-// [[Rcpp::export]]
-SEXP test_bson(SEXP field_name, SEXP field_values){
-	BSONObj _p = sexp_to_bson_obj(field_name, field_values);
-	std::vector<BSONElement> _v;
-	_p.elems(_v);
-	std::string _val;
-	for(int i = 0; i < _v.size(); ++i){
-	_v[i].Val(_val);
-	std::cout << "name: " << _v[i].fieldName() << ", value: "<< _val << std::endl;
-	}
-	return wrap(0);
-}
-
-
-// [[Rcpp::export]]
-SEXP iter_df(DataFrame df){
-	SEXP _col;
-	CharacterVector _names = df.names();
-	const char* _name;
-	std::vector<BSONElement> _v;
-	std::vector<std::vector<BSONElement> > _v_vec;
-	BSONObj _p;
-	// Convert the DF to BSONObj Array
-	for(int i = 0; i < df.length(); ++i){
-		_name = _names[i];
-		_col = df[_name];
-		for(int j = 0; j < df.nrows(); ++j){
-			_p = sexp_to_bson_obj(_name, _col);
-			_p.elems(_v);
-			_v_vec.push_back(_v);
-		}
-	}
-	std::cout << "point 1" << std::endl;
-	// Connection
-	mongo::client::initialize();
-	mongo::DBClientConnection conn;
-	try{
-		conn.connect("localhost");
-	} catch(const mongo::DBException &err){
-			std::cout << "caught " << err.what() << std::endl;
-	}
-	// Create a collection (table)
-	conn.createCollection("test.data");
-	BSONElement _el;
-	BSONObjBuilder _b;
-	for(int m = 0; m < _v_vec[0].size(); ++m){ // rows
-		for(int n = 0; n < _v_vec.size(); ++n){ // columns
-			std::cout << "point 2" << std::endl;
-			_el = _v_vec[m][n];
-			_b.append(_el); // issue
-		}
-		conn.insert("test.data", _b.obj());
-	}
-	// Print out table
-	std::auto_ptr<mongo::DBClientCursor> cursor = conn.query("test.data", BSONObj());
-	while(cursor->more()){
-		_p = cursor->next();
-		std::cout << "Sepal.Length: " << _p["Sepal.Length"].Number() << "\t" << 
-		", Sepal.Width: " << _p["Sepal.Width"].Number() << "\t" << ", Petal.Length: " << 
-			_p["Petal.Length"].Number() << "\t" << ", Petal.Width: " << 
-			_p["Petal.Width"].Number() << "\t" << _p["Species"] << std::endl;
-	}
+SEXP df_to_collection(DataFrame data_frame, CharacterVector collection){
 	
-	// Cleaning up
-	conn.dropCollection("test.data");
+	// Data frame related bits
+	SEXP _column; // to store a column of anonymous type
+	CharacterVector _names = data_frame.names(); // for the column names
+	const char* _name; // for a column name for writing to database
+	const char* _collection_name = collection[0];
+	int nCols = data_frame.size();
+	int nRows = data_frame.nrows();
 	
-	return wrap(0);
-}
-
-
-/*
-// [[Rcpp::export]]
-SEXP iter_df(DataFrame df){
-	SEXP _col;
-	CharacterVector _names = df.names();
-	const char* _name;
-	std::vector<BSONObj> _bv;
-	// Convert the DF to BSONObj Array
-	for(int i = 0; i < df.length(); ++i){
-		_name = _names[i];
-		_col = df[_name];
-		for(int j = 0; j < df.nrows(); ++j){
-			_bv.push_back(sexp_to_bson_obj(_name, _col));
-		}
-	}
+	BSONObj _p;
 	
 	// Connection
 	mongo::client::initialize();
-	mongo::DBClientConnection conn;
+	mongo::DBClientConnection _conn;
 	try{
-		conn.connect("localhost");
-	} catch(const mongo::DBException &err){
-			std::cout << "caught " << err.what() << std::endl;
+		_conn.connect("localhost");
+	} catch(const mongo::DBException &_err){
+			std::cout << "caught " << _err.what() << std::endl;
 	}
 	// Create a collection (table)
-	conn.createCollection("test.data");
-	BSONObj _p;
-	BSONElement _el;
-	BSONObjBuilder _b;
-	for(int m = 0; m < _bv[0].nFields(); ++m){
-		for(int n = 0; n < _bv.size(); ++n){
-			//_p = _bv[n];
-			//_el = _bv[n][m];
-			_b.append(_bv[n][m]); // issue
-		}
-		conn.insert("test.data", _b.obj());
-	}
-	// Print out table
-	std::auto_ptr<mongo::DBClientCursor> cursor = conn.query("test.data", BSONObj());
-	while(cursor->more()){
-		_p = cursor->next();
-		std::cout << "Sepal.Length: " << _p["Sepal.Length"].Number() << "\t" << 
-		", Sepal.Width: " << _p["Sepal.Width"].Number() << "\t" << ", Petal.Length: " << 
-			_p["Petal.Length"].Number() << "\t" << ", Petal.Width: " << 
-			_p["Petal.Width"].Number() << "\t" << _p["Species"] << std::endl;
-	}
+	_conn.createCollection(_collection_name);
 	
-	// Cleaning up
-	conn.dropCollection("test.data");
+	// This for loop runs over the rows and columns.
+	for(int i = 0; i < nRows; ++i){
+		//std::cout << "Row: " << i << std::endl;
+		BSONObjBuilder _bM;
+		for(int j = 0; j < nCols; ++j){
+			_name = _names[j];
+			_column = data_frame[_name];
+			//std::cout << "Column: " << j << std::endl;	
+			if(TYPEOF(_column) == REALSXP){
+				NumericVector _field(_column);
+				_bM.append(_name, _field[i]);
+			} else if(TYPEOF(_column) == INTSXP){
+				IntegerVector _field(_column);
+				_bM.append(_name, _field[i]);
+			} else if(TYPEOF(_column) == LGLSXP){
+				IntegerVector _field(_column);
+				_bM.append(_name, _field[i]);
+			} else { // catch all convert to const char
+				CharacterVector _field(_column);
+				const char* _el;
+				_el = _field[i];
+				_bM.append(_name, _el);
+			}
+		}
+		_p = _bM.obj();
+		_conn.insert(_collection_name, _p);
+	}
 	
 	return wrap(0);
 }
-*/
+
+//[[Rcpp::export]]
+SEXP collection_to_df(CharacterVector collection_name){
+	
+	const char *_collection_name = collection_name[0];
+	
+	// Connection
+	mongo::client::initialize();
+	mongo::DBClientConnection _conn;
+	try{
+		_conn.connect("localhost");
+	} catch(const mongo::DBException &_err){
+			std::cout << "caught " << _err.what() << std::endl;
+	}
+	// Create a collection (table)
+	_conn.createCollection(_collection_name);
+	
+	std::auto_ptr<mongo::DBClientCursor> _cursor = _conn.query(_collection_name, BSONObj());
+	
+	BSONObj _p;
+	_p = _cursor->next();
+	std::set<std::string> _field_names;
+	int _nCol = _p.nFields();
+	int _status = _p.getFieldNames(_field_names);
+	std::string _name;
+	const char *_nm;
+	std::vector<const char*> _names;
+	std::vector<std::string> _col_names;
+	const char *_item;
+	List _list;
+	int n = 0;
+	
+	
+	for(BSONObj::iterator it = _p.begin(); it.more(); ){
+		BSONElement _bEl = it.next();
+		_nm = _bEl.fieldName();
+		_name = _nm;
+		_col_names.push_back(_name);
+		if(_bEl.isNumber()){
+			NumericVector _column;
+			_column.push_back(_bEl.number());
+			_list[_name] = _column;
+		}else{
+			CharacterVector _column;
+			_column.push_back(_bEl.str());
+			_list[_name] = _column;
+		}
+	}
+	
+	while(_cursor->more()){
+		_p = _cursor->next();
+		for(BSONObj::iterator it = _p.begin(); it.more(); ){
+			BSONElement _bEl = it.next();
+			_nm = _bEl.fieldName();
+			_name = _nm;
+			//_col_names.push_back(_name);
+			if(_bEl.isNumber()){
+				NumericVector _column = _list[_name];
+				_column.push_back(_bEl.number());
+				_list[_name] = _column;
+			}else{
+				CharacterVector _column = _list[_name];
+				_column.push_back(_bEl.str());
+				_list[_name] = _column;
+			}
+		}
+	}
+	
+	List _list_out;
+	for(int i = 1; i < _col_names.size(); ++i){
+		_list_out[_col_names[i]] = _list[_col_names[i]];
+	}
+	
+	DataFrame _data_frame(_list_out);
+	return wrap(_data_frame);
+}
 
 // [[Rcpp::export]]
-SEXP test(){
-	CharacterVector x(1);
-	x[0] = "HW!";
-	return x;
+SEXP delete_collection(CharacterVector collection_name){
+	const char* _collection_name = collection_name[0];
+	mongo::client::initialize();
+	mongo::DBClientConnection _conn;
+	try{
+		_conn.connect("localhost");
+	} catch(const mongo::DBException &_err){
+			std::cout << "caught " << _err.what() << std::endl;
+	}
+	
+	_conn.dropCollection(_collection_name);
+	return wrap(0);
 }
+
 
 /*
 # R code
-# This is currently broken
 require(Rcpp)
 flgString <- '-pthread -lmongoclient -lboost_thread -lboost_system -lboost_regex'
 Sys.setenv("PKG_CXXFLAGS"=flgString)
@@ -269,5 +180,7 @@ Sys.setenv("PKG_LIBS"=flgString)
 sourceCpp("mongo_rcpp_2.cpp")
 iris2 <- iris
 iris2$Species <- as.character(iris2$Species)
-iter_df(iris2)
+df_to_collection(iris2[1:5,], "test.data")
+collection_to_df("test.data")
+delete_collection("test.data")
 */
